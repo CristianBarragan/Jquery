b638067ae89052ba815f7e155a47f307a3da4605
cristianbarragan <cristian.barragan@gmail.com>
2017-02-08 09:20:55 +1300

    First Commit

---
+++ ProductWebApi/Controllers/ProductController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Http;
+using ProductWebApi.Data;
+using System.Net.Http;
+using System.Net;
+
+namespace ProductWebApi.Controllers
+{
+    public class ProductController : ApiController
+    {
+        private WebApiEntities context;
+        //private List<Product> Products = new List<Product>();
+
+        public ProductController()
+        {
+            context = new WebApiEntities();
+            //Products.Add(new Product { Id = 1, Name = "Product1", Quantity = 20 });
+            //Products.Add(new Product { Id = 2, Name = "Product2", Quantity = 10 });
+            //Products.Add(new Product { Id = 3, Name = "Product3", Quantity = 30 });
+        }
+
+        public IEnumerable<Product> Get()
+        {
+            return context.Products;
+        }
+
+        public Product Get(int id)
+        {
+            return context.Products.FirstOrDefault(p => p.Id == id);
+        }
+
+        public void Post(Product product)
+        {
+            context.Products.AddObject(product);
+            context.SaveChanges();
+        }
+
+        public void Put([FromUri]int id, [FromBody]Product product)
+        {
+            context.Products.ApplyCurrentValues(product);
+            context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            context.Products.DeleteObject(context.Products.FirstOrDefault(p => p.Id == id));
+            context.SaveChanges();
+        }
+
+        public HttpResponseMessage Options()
+        {
+            return new HttpResponseMessage { StatusCode = HttpStatusCode.OK };
+        }
+
+    }
+}
---
+++ ProductWebApp/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProductWebApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        //
+        // GET: /Product/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET: /Product/Details/5
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+    }
+}
