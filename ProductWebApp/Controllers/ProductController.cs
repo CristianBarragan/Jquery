0f9a952e4e76099f0bc21e5c8880b78497ce7126
cristianbarragan <cristian.barragan@gmail.com>
2017-02-09 11:13:38 +1300

    Adding update

--- ProductWebApp/Controllers/ProductController.cs
+++ ProductWebApp/Controllers/ProductController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ProductWebApp.Models;
+using System.Net.Http;
 
 namespace ProductWebApp.Controllers
 {
@@ -17,11 +19,27 @@ namespace ProductWebApp.Controllers
         }
 
         //
-        // GET: /Product/Details/5
 
         public ActionResult Add()
         {
             return View();
+        }
+
+        public ActionResult Update(int id)
+        {
+            HttpClient client = new HttpClient();
+            Product product = null;
+            client.BaseAddress = new Uri("http://localhost:31486/api/");
+            HttpResponseMessage response = client.GetAsync("Product/" + id).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                 product = response.Content.ReadAsAsync<Product>().Result;
+            }
+            else
+            {
+                //Something has gone wrong, handle it here
+            }
+            return View(product);
         }
     }
 }
---
+++ ProductWebApp/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProductWebApp.Models
+{
+    public class Product
+    {
+        public int Id { set; get; }
+        public string Name { set; get; }
+        public int Quantity { set; get; }
+    }
+}
